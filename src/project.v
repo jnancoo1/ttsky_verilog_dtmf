c691dcdfb0160fef80472b1a38ca4aa52d25c2ff
jnancoo1 <you@example.com>
2026-05-08 12:12:51 -0400

    verilog added

--- src/project.v
+++ src/project.v
@@ -1,27 +1,71 @@
-/*
- * Copyright (c) 2024 Your Name
- * SPDX-License-Identifier: Apache-2.0
- */
+// File project.vhdl translated with vhd2vl 3.0 VHDL to Verilog RTL translator
+// vhd2vl settings:
+//  * Verilog Module Declaration Style: 2001
 
-`default_nettype none
+// vhd2vl is Free (libre) Software:
+//   Copyright (C) 2001-2023 Vincenzo Liguori - Ocean Logic Pty Ltd
+//     http://www.ocean-logic.com
+//   Modifications Copyright (C) 2006 Mark Gonzales - PMC Sierra Inc
+//   Modifications (C) 2010 Shankar Giri
+//   Modifications Copyright (C) 2002-2023 Larry Doolittle
+//     http://doolittle.icarus.com/~larry/vhd2vl/
+//   Modifications (C) 2017 Rodrigo A. Melo
+//
+//   vhd2vl comes with ABSOLUTELY NO WARRANTY.  Always check the resulting
+//   Verilog for correctness, ideally with a formal verification tool.
+//
+//   You are welcome to redistribute vhd2vl under certain conditions.
+//   See the license (GPLv2) file included with the source for details.
 
-module tt_um_example (
-    input  wire [7:0] ui_in,    // Dedicated inputs
-    output wire [7:0] uo_out,   // Dedicated outputs
-    input  wire [7:0] uio_in,   // IOs: Input path
-    output wire [7:0] uio_out,  // IOs: Output path
-    output wire [7:0] uio_oe,   // IOs: Enable path (active high: 0=input, 1=output)
-    input  wire       ena,      // always 1 when the design is powered, so you can ignore it
-    input  wire       clk,      // clock
-    input  wire       rst_n     // reset_n - low to reset
+// The result of translation follows.  Its copyright status should be
+// considered unchanged from the original VHDL.
+
+// no timescale needed
+
+module tt_um_dtmf_gen(
+input wire [7:0] ui_in,
+output wire [7:0] uo_out,
+input wire [7:0] uio_in,
+output wire [7:0] uio_out,
+output wire [7:0] uio_oe,
+input wire ena,
+input wire clk,
+input wire rst_n
 );
 
-  // All output pins must be assigned. If not used, assign to 0.
-  assign uo_out  = ui_in + uio_in;  // Example: ou_out is the sum of ui_in and uio_in
-  assign uio_out = 0;
-  assign uio_oe  = 0;
 
-  // List all unused inputs to prevent warnings
-  wire _unused = &{ena, clk, rst_n, 1'b0};
+
+
+wire reset_s;
+wire key_press_s;
+wire dtmf_en_s;
+wire dial_en_s;
+wire [3:0] key_code_s;
+wire fh_s;
+wire fl_s;
+
+  // TinyTapeout reset is ACTIVE LOW
+  assign reset_s =  ~rst_n;
+  // Example pin mapping
+  assign key_press_s = ui_in[0];
+  assign key_code_s = ui_in[4:1];
+  assign dtmf_en_s = ui_in[5];
+  assign dial_en_s = ui_in[6];
+  // DATAPATH INSTANCE
+  datapath UUT(
+    .reset(reset_s),
+    .key_press(key_press_s),
+    .key_code(key_code_s),
+    .reference_clk(clk),
+    .dtmf_en(dtmf_en_s),
+    .dial_en(dial_en_s),
+    .Fh(fh_s),
+    .Fl(fl_s));
+
+  assign uo_out[0] = fh_s;
+  assign uo_out[1] = fl_s;
+  assign uo_out[7:2] = {6{1'b0}};
+  assign uio_out = {8{1'b0}};
+  assign uio_oe = {8{1'b0}};
 
 endmodule
